d22b814a3ca7025d4b674a6c0b9bcc34031818da
Aishwarya Naidu <aishwaryanaidu@aishwaryas-mbp.notrust.clients.wireless.pfw.edu>
2022-04-03 02:57:53 -0400

    API integration

--- frontend/src/components/FakeNews.js
+++ frontend/src/components/FakeNews.js
@@ -6,48 +6,56 @@ import { Input, Select, Row, Col, Button } from 'antd';
 
 const { TextArea } = Input;
 
+// open -n -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --user-data-dir="/tmp/chrome_dev_test" --disable-web-security
+
 export const FakeNews = () => {
 
-    const [ inputText, setInputText ] = useState("");
-    const [ output, setOutput ] = useState("");
+    const [inputText, setInputText] = useState("");
+    const [output, setOutput] = useState("");
 
     const check = () => {
-        console.log(inputText);
-        // fetch('http://e6a4-35-221-218-113.ngrok.io', { 
-        //   method: 'POST',
-        //   headers: {
-        //   },
-        //   body: { input: inputText }
-        // }).then(response =>  response.json())
-        // .then(resData => { 
-        //     setOutput(resData.predictions)
-        // }
-        // ).catch(
-        //   error => console.log(error)
-        // );
-      };
+        fetch('http://7994-34-125-236-23.ngrok.io', { 
+          method: 'POST',
+        //   mode: 'cors',
+          headers: {
+            'Content-Type':'application/json'
+          },
+          body: JSON.stringify({ "input": inputText })
+        }).then(response => response.json())
+        .then(resData => {
+            setOutput(resData.predictions)
+        }).catch(error => console.log(error))
+    };
 
     const handleTextChange = (event) => {
         setInputText(event.target.value);
     }
 
+    console.log(output)
+
     return (
         <div>
             <Row gutter={24}>
                 <Col span={12}>
-                    <img style={{ height:"100vh", width: "57vw" }} src={require('../images/fake.jpeg')} />
+                    <img style={{ height: "100vh", width: "57vw" }} src={require('../images/fake.jpeg')} />
                 </Col>
                 <Col span={2}></Col>
                 <Col span={10}>
                     <div style={{ marginTop: "15%" }}>
-                    <p style={{ fontSize: "20pt", color: "#CD5465", alignContent: "center", marginLeft: "25%"}}>Fake News Detection</p>
-                    <div style={{ marginTop: '10%' }}>
-                    <TextArea maxLength={100} style={{ height: 400, width: 550, border: '2px solid #CD5465'  }} value={inputText} onChange={handleTextChange}/>
-                    <Button type="primary" shape="round" style={{ width: "30%", marginLeft: "35%", marginTop: "5%", height: "15%", backgroundColor: !!!inputText? "#B1B1B1": "#CD5465", border: !!!inputText? "" : "1px solid #CD5465" }} onClick={check} disabled={!!!inputText}>
-                        Check
-                    </Button>
+                        <p style={{ fontSize: "20pt", color: "#CD5465", alignContent: "center", marginLeft: "25%" }}>Fake News Detection</p>
+                        <div style={{ marginTop: '10%' }}>
+                            <TextArea maxLength={100} style={{ height: 400, width: 550, border: '2px solid #CD5465' }} value={inputText} onChange={handleTextChange} />
+                            <Button type="primary" shape="round" style={{ width: "30%", marginLeft: "35%", marginTop: "5%", height: "20%", backgroundColor: !!!inputText ? "#B1B1B1" : "#CD5465", border: !!!inputText ? "" : "1px solid #CD5465" }} onClick={check} disabled={!!!inputText}>
+                                Check
+                            </Button>
+                        </div>
                     </div>
-                    </div>
+                    {!!output && output == "1" &&
+                        <p>The input text is real</p>
+                    }
+                    {!!output && output == "0" &&
+                        <p>The input text is fake</p>
+                    }
                 </Col>
             </Row>
         </div>
